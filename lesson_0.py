0ef096fa7b41d9f77e149f70dec1ffc3c6834c0c
abdurazzak2002 <toloev.abi97@gmail.com>
2021-09-18 13:41:20 +0600

    start

---
+++ lesson_0.py
@@ -0,0 +1,12 @@
+#input - ввод инфы через терминал
+#name = input('Введите свое имя: ')
+# = оператор присваивания 
+#print('Привет,',name,'!')
+#Арифметические операторы + - * / 
+
+print('a*b-c')
+number1 = int(input('Введите цифру а: '))
+number2 = int(input('Введите цифру b: '))
+number3 = int(input('Введите цифру c: '))
+#print(number1*number2-number3)
+print(type(number1))
